da2a30333d7aaf69016ac5edb1ed4c79073ae164
zakizaki1147 <zakisantoso47@gmail.com>
2025-12-07 21:03:46 +0700

    first commit

---
+++ src/utils/postCardStyles.js
@@ -0,0 +1,146 @@
+export const postCardStyles = {
+    "Absurd & Uncanny": {
+        bg: "bg-purple-secondary",
+        border: "border-purple-primary",
+        tagBg: "bg-purple-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "Accidents & Incidents": {
+        bg: "bg-red-secondary",
+        border: "border-red-primary",
+        tagBg: "bg-red-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "Foods & Drinks": {
+        bg: "bg-orange-secondary",
+        border: "border-orange-primary",
+        tagBg: "bg-orange-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "History": {
+        bg: "bg-brown-secondary",
+        border: "border-brown-primary",
+        tagBg: "bg-brown-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Hobbies & Collections": {
+        bg: "bg-magenta-secondary",
+        border: "border-magenta-primary",
+        tagBg: "bg-magenta-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "Humor & Satire": {
+        bg: "bg-lightgreen-secondary",
+        border: "border-lightgreen-primary",
+        tagBg: "bg-lightgreen-primary",
+        tagText: "text-black",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Internet & Technology": {
+        bg: "bg-darkblue-secondary",
+        border: "border-darkblue-primary",
+        tagBg: "bg-darkblue-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Love & Confessions": {
+        bg: "bg-pink-secondary",
+        border: "border-pink-primary",
+        tagBg: "bg-pink-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "[CENSORED]": {
+        bg: "bg-red-tertiary",
+        border: "border-red-primary",
+        tagBg: "bg-red-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Personal Experiences": {
+        bg: "bg-gray-primary",
+        border: "border-black",
+        tagBg: "bg-black",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "Pets & Animals": {
+        bg: "bg-yellow-secondary",
+        border: "border-yellow-primary",
+        tagBg: "bg-yellow-primary",
+        tagText: "text-black",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Politics & [REDACTED]": {
+        bg: "bg-lime-secondary",
+        border: "border-lime-primary",
+        tagBg: "bg-lime-primary",
+        tagText: "text-black",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Sciences & Nature": {
+        bg: "bg-darkgreen-secondary",
+        border: "border-darkgreen-primary",
+        tagBg: "bg-darkgreen-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Space & Extraterrestrials": {
+        bg: "bg-lightblue-secondary",
+        border: "border-lightblue-primary",
+        tagBg: "bg-lightblue-primary",
+        tagText: "text-black",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    },
+    "Supernatural & Occultism": {
+        bg: "bg-black",
+        border: "border-red-primary",
+        tagBg: "bg-red-primary",
+        tagText: "text-white",
+        text: "text-red-primary",
+        dateText: "text-gray-primary"
+    },
+    "[CORRUPTED]": {
+        bg: "bg-black",
+        border: "border-white",
+        tagBg: "bg-white",
+        tagText: "text-black",
+        text: "text-white",
+        dateText: "text-gray-primary"
+    },
+    "Wonderhoy!": {
+        bg: "bg-pink-secondary",
+        border: "border-pink-primary",
+        tagBg: "bg-pink-primary",
+        tagText: "text-white",
+        text: "text-black",
+        dateText: "text-white"
+    },
+    "default": {
+        bg: "bg-gray-primary",
+        border: "bg-black",
+        tagBg: "bg-black",
+        tagText: "text-black",
+        text: "text-black",
+        dateText: "text-gray-primary"
+    }
+};
