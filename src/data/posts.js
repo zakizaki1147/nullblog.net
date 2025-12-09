a3a80555df57de7bc7d7a6814ece33d45c47e71c
zakizaki1147 <zakisantoso47@gmail.com>
2025-12-09 11:27:50 +0700

    feat: add topic page

---
+++ src/data/posts.js
@@ -0,0 +1,432 @@
+// Absurd & Uncanny Images
+import WorldEgg from "../assets/Absurd&Uncanny/WorldEgg.webp";
+import Wonderhoy from "../assets/Absurd&Uncanny/Wonderhoy!.webp";
+// Accidents & Incidents Images
+import BombingIncident from "../assets/Accidents&Incidents/BombingIncident.webp";
+import FlightCrash from "../assets/Accidents&Incidents/FlightCrash.webp";
+import PoliceBrutality from "../assets/Accidents&Incidents/PoliceBrutality.webp";
+// Foods & Drinks Images
+import TheMostDeliciousFood from "../assets/Foods&Drinks/TheMostDeliciousFood.webp";
+// History
+import CloudBomb from "../assets/History/CloudBomb.webp";
+import Nigersaurus from "../assets/History/Nigersaurus.webp";
+// Hobbies & Collections Images
+import TheHeroAndTheHater from "../assets/Hobbies&Collections/TheHeroAndTheHater.webp";
+// Humor & Satire Images
+import AgarthaAhh from "../assets/Humor&Satire/AgarthaAhh.webp";
+import MyMomIsKindaHomeless from "../assets/Humor&Satire/MyMomIsKindaHomeless.gif";
+import ZiasAndBlou from "../assets/Humor&Satire/ZiasAndBlou.webp";
+// Internet & Technology Images
+// Love & Confessions
+// Murders & Assassinations
+// Personal Experiences
+// Pets & Animals
+import CatSlayer from "../assets/Pets&Animals/CatSlayer.webp";
+import MissingCat from "../assets/Pets&Animals/MissingCat.webp";
+// Politics & Elites
+// Sciences & Nature
+// Space & Extraterrestrials
+import PaleBlueDot from "../assets/Space&Extraterrestrials/PaleBlueDot.webp";
+// Supernatural & Occultism
+// Lost Media
+import NotWorldEgg from "../assets/LostMedia/NotWorldEgg.webp";
+import TelevisionGhost from "../assets/LostMedia/TelevisionGhost.webp";
+
+export const posts = [
+  {
+    id: 1,
+    username: "world_record_egg",
+    date: "3/3/3",
+    topic: "Absurd & Uncanny",
+    title: "Just an Egg",
+    description:
+      "Let’s set a world record together and get the most upvoted post on Nullblog. Beating the current world record held by Kylie Jenner (18 thousand upvoted)! We got this 🙌 <br> <br> #UpvoteTheEgg #EggSoldiers #EggGang",
+    image: WorldEgg,
+    blurred: false,
+    upvotes: "28.4K",
+    comments: "331",
+  },
+  {
+    id: 2,
+    username: "Wonderhoy!",
+    date: "3/3/3",
+    topic: "Wonderhoy!",
+    title: "Wonderhoy!",
+    description: "Wonderhoy!",
+    image: Wonderhoy,
+    blurred: false,
+    upvotes: "22.7K",
+    comments: "948",
+  },
+  {
+    id: 3,
+    username: "meme_courses",
+    date: "3/3/3",
+    topic: "Humor & Satire",
+    title: "Generational Meme",
+    description:
+      "On a recent stream, one of the most popular streamers, YouDemonstrateVelocity, made a very funny face after hearing his viewer say “My mom is kinda homeless” while playing a multiplayer game. Hearing that, Velocity tried his best not to laugh to respect his viewer’s condition. <br> What situation would make you guys make a face like this?",
+    image: MyMomIsKindaHomeless,
+    blurred: false,
+    upvotes: "21.2K",
+    comments: "289",
+  },
+  {
+    id: 4,
+    username: "c4c4krikri",
+    date: "3/3/3",
+    topic: "Pets & Animals",
+    title: "Meet the Slayer!",
+    description:
+      "Ayyyy finally I have my own cat after wanting it for a long time😭 btw his name is Dragon SlayerX!",
+    image: CatSlayer,
+    blurred: false,
+    upvotes: "20.3K",
+    comments: "105",
+  },
+  {
+    id: 5,
+    username: "matt",
+    date: "3/3/3",
+    topic: "Love & Confessions",
+    title: "About You",
+    description:
+      "I know a place. It's somewhere I go when I need to remember your face. <br> We get married in our heads. Something to do whilst we try to recall how we met. <br> <br> Do you think I have forgotten? <br> Do you think I have forgotten about you? <br> <br> You and I, were alive. With nothing to do I could lay and just look in your eyes. <br> Wait and pretend, hold on and hope that we'll find our way back in the end. <br> <br> Do you think I have forgotten? <br> Do you think I have forgotten about you?",
+    upvotes: "17.1K",
+    comments: "247",
+  },
+  {
+    id: 6,
+    username: "ash",
+    date: "3/3/3",
+    topic: "Hobbies & Collections",
+    title: "I Got the Rarest Hamamon Card!",
+    description:
+      "AHAHAHAHA I got the legendary Hamamon card, the Hero and the Hater! The only one in this world! HAHAHAHAHAHAHA",
+    image: TheHeroAndTheHater,
+    blurred: true,
+    upvotes: "15.4K",
+    comments: "276",
+  },
+  {
+    id: 7,
+    username: "SC_star",
+    date: "3/3/3",
+    topic: "Space & Extraterrestrials",
+    title: "The Pale Blue Dot",
+    description:
+      "This is a photograph of our planet taken on #/#/89 by the Star Constellation’s space probe from far, far away. In the photograph, our planet appears as a tiny dot against the vastness of space, among bands of sunlight reflected by the camera. <br> <br> This photograph is considered as one of the most important photograph ever taken in the world because of its historical aspect, its influence on the advancement of space technology, and its role as “Family Portrait” of the XXXlings.",
+    image: PaleBlueDot,
+    blurred: false,
+    upvotes: "15K",
+    comments: "304",
+  },
+  {
+    id: 8,
+    username: "JayMaskell",
+    date: "3/3/3",
+    topic: "Supernatural & Occultism",
+    title: "Accidently Saw a Cult Ritual",
+    description:
+      "No shit. I saw a group of people doing some strange things in the woods near my house. They burned someone alive. <br> <br> Yesterday, during night, me and my brother heard some loud noises from the forest behind our house. Something like loud bush rumble. Then I wanted to went out of the house to check the sound. I was afraid it was the sound of a bear trying to get out of the forest. My brother insisted on coming along because he was afraid of being alone at home. Our parents still hadn't come home from work. I took my flashlight, locked the door, and went straight to woods with my brother. <br> <br> In the forest, we did not see any bears, or any wild animals. But we saw many- a group of people in white robe, forming a circle. We are in confusion, but also curious. I turned off my flashlight and we went into full silent mode to see what they would do. But, somehow, my brother has been acting weird recently.",
+    upvotes: "14.9K",
+    comments: "211",
+  },
+  {
+    id: 9,
+    username: "FingeSD",
+    date: "3/3/3",
+    topic: "[CENSORED]",
+    title: "Live Streaming of the Dead",
+    description:
+      "When I was doomscrolling in ###, I saw a live streaming of a girl like in her early 20s preparing for something. She brought in some familiar items for... committing suicide, such as a rope and a chair. I kept watching the stream, wondering if this was a real situation or just a cheap prank. As the stream progressed, the number of viewers steadily grew. Not many of them even asked for the woman to actually commit suicide. And yup, after the viewers reached 10.000, she actually committed suicide by jumping from the chair with the rope tied around her neck. She was... hanging, in front of the camera.",
+    upvotes: "14K",
+    comments: "275",
+  },
+  {
+    id: 10,
+    username: "kurniawan_henry",
+    date: "3/3/3",
+    topic: "Internet & Technology",
+    title: "My Friend Disappeared After Opening a Website Link",
+    description:
+      "Previously, me and one of my friend found a suspicious link from diving deep into the internet. We found the link just simply appeared without any context. Then yesterday, my friend said he wanted to open the link, but I didn’t want to open it because I was afraid something would happen to my data. And now... I can't find any news from my friend. I tried to visit his house but there was no answer. He's gone...  <br> <br> Here is the link, be careful https://bit.ly/43Du7Eh",
+    upvotes: "12.5K",
+    comments: "337",
+  },
+  {
+    id: 11,
+    username: "mamank_carlos_kun",
+    date: "3/3/3",
+    topic: "Foods & Drinks",
+    title: "What is the Most Delicious Food of All Time?",
+    description:
+      "Let’s give your opinion about the most delicious food of all time! Mine is rendang! 😋",
+    upvotes: "12.1K",
+    comments: "352",
+  },
+  {
+    id: 12,
+    username: "HiNaShiKi",
+    date: "3/3/3",
+    topic: "History",
+    title: "Atomic Bombing of ###",
+    description:
+      "## years ago, an atomic bomb was detonated in the city of ### by ### to “end” the ### War.",
+    image: CloudBomb,
+    blurred: false,
+    upvotes: "10.6K",
+    comments: "243",
+  },
+  {
+    id: 13,
+    username: "xiachun",
+    date: "3/3/3",
+    topic: "Accidents & Incidents",
+    title: "Flight Crash Captured in Dashcam",
+    description:
+      "A horrifying photo of a crashing plane was captured in a dashcam car on #/#/###. Trans### Airways was a domestic flight from ### to ###. Because of an engine failure, the plane crashed into the ### River after crashing its left wing to a local highway. On board were 58 people, 15 of whom survived with injuries.",
+    image: FlightCrash,
+    blurred: true,
+    upvotes: "10.3K",
+    comments: "292",
+  },
+  {
+    id: 14,
+    username: "a",
+    date: "3/3/3",
+    topic: "Sciences & Nature",
+    title: "Yggdrasil, the Tree of Life",
+    description: "a",
+    upvotes: "9.7K",
+    comments: "170",
+  },
+  {
+    id: 15,
+    username: "SOG",
+    date: "3/3/3",
+    topic: "Politics & [REDACTED]",
+    title: "The Situation of Salvatore Family",
+    description:
+      "The most respected family in the world, <strong>the Salvatore Family</strong>, now in a worrying condition. After the recent death of the 7th head of the family who was also the leader of ### and a father, Claudio Salvatore, the country and the family is in the “vacuum of power” condition. There was no clear family heir among Claudio’s three children. When the third child doesn’t give any sh*t, the first and second child are in conflict arguing about who is worthy to be the 8th head of the family. <br> <br> The first child, Ulrico Salvatore is known as a cruel leader but he is a firm person and very strict to the rules. The second child, Federico Salvatore is known as a kind leader, very protective of his people, but he can be too soft sometimes. The third child, Claudia Salvatore is just happen to be there. <br> <br> Because of this “vacuum of power” and family conflict condition, the situation of ###’s people is getting heated too. Many people support Federico to be the 8th leader, but the people who support Ulrico are also no less numerous. Well, there are some people that don’t give any sh*t just like Claudia.",
+    upvotes: "6.9K",
+    comments: "157",
+  },
+  {
+    id: 16,
+    username: "NatLivs",
+    date: "3/3/3",
+    topic: "Personal Experiences",
+    title: "Thoughts on This Website's Recent Posts?",
+    description:
+      "Huftt, this website’s content are getting weirder by the day... <br> Recently, many users have been posting many weird, disturbing, and NSFW contents, even though this website originally has general to wholesome content. <br> What do you guys think about this situation?",
+    upvotes: "3.4K",
+    comments: "225",
+  },
+  // out of popular posts
+  {
+    id: 17,
+    username: "hood",
+    date: "3/3/3",
+    topic: "History",
+    title: "Thoughts on This Dinosaur⁉️⁉️",
+    description:
+      "While I was browsing through the dinosaurs that once existed, the name of a very interesting dinosaur randomly popped up. Wdyt⁉️",
+    image: Nigersaurus,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 18,
+    username: "whiteboywannabe",
+    date: "3/3/3",
+    topic: "Humor & Satire",
+    title: "Agartha vs. Rooftile",
+    description:
+      "Type shii u will see before entering Agartha lol <br> Bro ain’t surviving from a rooftile💀💀",
+    image: AgarthaAhh,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 19,
+    username: "zbzbzbz",
+    date: "3/3/3",
+    topic: "Humor & Satire",
+    title: "Greatest Scientist of the History⁉️",
+    description:
+      "I just watched a video featuring these big brains discussing about which one is faster between light and sound. The results were truly fascinating. Could these two be the greatest scientists of the history!?",
+    image: ZiasAndBlou,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 20,
+    username: "tyr1",
+    date: "3/3/3",
+    topic: "Accidents & Incidents",
+    title: "### Bombing",
+    description:
+      "a",
+    image: BombingIncident,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 21,
+    username: "AK",
+    date: "3/3/3",
+    topic: "Accidents & Incidents",
+    title: "a",
+    description:
+      "a",
+    image: PoliceBrutality,
+    blurred: true,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 22,
+    username: "caneater",
+    date: "3/3/3",
+    topic: "Foods & Drinks",
+    title: "The Most Delicious Food That Only Appears Once a Year",
+    description:
+      "Without a doubt, this is the most delicious food I have ever eaten! But sadly, this food only appears every once a year at a celebration event in my religion...",
+    image: TheMostDeliciousFood,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 23,
+    username: "1ibr4a",
+    date: "3/3/3",
+    topic: "Pets & Animals",
+    title: "My Cat is Missing T_T",
+    description:
+      "HELP!!! Please, if anyone sees my cat like the one in the picture I sent, please let me know! The last time I saw her was around my neighborhood in ###!",
+    image: MissingCat,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 24,
+    username: "not_world_record_egg",
+    date: "3/3/3",
+    topic: "[CORRUPTED]",
+    title: "Not Just an Egg",
+    description:
+      "Let’s set a world record together and get the most downvoted post on Nullblog. Beating the current world record held by Kylie Jenner (18 thousand downvoted)! We got this 🙌 <br> <br> #DownvoteTheEgg #EggSoldiers #EggGang",
+    image: NotWorldEgg,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 25,
+    username: "ghost_hunter",
+    date: "3/3/3",
+    topic: "[CORRUPTED]",
+    title: "Ghost Sightings in Television Broadcast?",
+    description:
+      "When I was a kid, every time the star set, I would always look away from the television. Why? Because I’m pretty sure that I had seen a ghost on a television program that always aired every sunset. But now, the television program is no longer aired. When I dived into the internet, I could only find the photo I attached. I couldn't find the entire or just a piece recording of the television program that showed the ghost figure. I'm curious if you guys have also seen it or even have the entire recording?",
+    image: TelevisionGhost,
+    blurred: false,
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 26,
+    username: "soysad",
+    date: "3/3/3",
+    topic: "Foods & Drinks",
+    title: "Beware with This Chicken Restaurant!",
+    description:
+      "Beware with the Nuclear Smashed Chicken in ###! I repeat, beware with the Nuclear Smashed Chicken! I got a stomach ache from eating that chicken on level 1 yesterday. LEVEL 1! NO SH*T! This is a warning.",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 27,
+    username: "",
+    date: "3/3/3",
+    topic: "History",
+    title: "This World... is not the First?",
+    description:
+      "With the presence of the ### race, the humans with superpowers, they have done so much things that have impacted this world, both good and bad. The ### race claims to have been created by “The Creator”, even though they have never seen “The Creator”’s form. Is “The Creator” real? Where did “The Creator” come from? <br><br> I have a theory. “The Creator” was come from the previous world, and this world is definitely not the first. But why did “The Creator” created this new world? What happened to the previous world? <br><br> I’m sure that the previous world was over, whether it was destroyed or just simply removed by “The Creator”. The previous world is no longer exists. The ### race from the previous world has finished or achieved something for the purpose of why are the ### race is created by “The Creator”. The ### race are always be the center of this world, while us, the human race are just a viewer in their story.",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 28,
+    username: "RicoRecing",
+    date: "3/3/3",
+    topic: "Personal Experiences",
+    title: "First Time Working💀💀",
+    description:
+      "For the first time, I have a work, as a keeper in my neighbor's chicken coop lol. Wish me luck",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 27,
+    username: "caneater",
+    date: "3/3/3",
+    topic: "Personal Experiences",
+    title: "Graduation Day",
+    description:
+      "The graduation day of my school has come. I'm not ready. There are so many memories that created from that school T_T <br> My joyful days with my friends also my crush will be over... <br> Guess I'll gtg rn.",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 27,
+    username: "",
+    date: "3/3/3",
+    topic: "Personal Experiences",
+    title: "Crazy Sleeping Experience",
+    description:
+      "WTF!!! I've been asleep for 29 hours NONSTOP! <br> Yesterday, no, two days ago, I was very tired after working overtime for several days. After the work was finished, I went straight home and fell asleep soundly. When I woke up and checked my phone, I realized I had been asleep for over a day! Damn, maybe I was just too tired XD <br> This is a crazy experience for sure lol",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 28,
+    username: "",
+    date: "3/3/3",
+    topic: "Space & Extraterrestrials",
+    title: "I Got Abducted by Aliens...",
+    description:
+      "",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 29,
+    username: "caneater",
+    date: "3/3/3",
+    topic: "Hobbies & Collections",
+    title: "My First Action Figure! :)",
+    description:
+      "After getting good grades, I got my first action figure as a birthday present! He is ### from ###! Thanks to mum and dad for buying me this! ❤️❤️",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+  {
+    id: 30,
+    username: "",
+    date: "3/3/3",
+    topic: "[CENSORED]",
+    title: "Mysterious Case of 13 Bones in Neo-Karta",
+    description:
+      "",
+    upvotes: "18.7K",
+    comments: "948",
+  },
+];
---
+++ src/utils/topicSlugMap.js
@@ -0,0 +1,18 @@
+export const topicSlugMap = {
+  "absurd-uncanny": "Absurd & Uncanny",
+  "accidents-incidents": "Accidents & Incidents",
+  "foods-drinks": "Foods & Drinks",
+  "history": "History",
+  "hobbies-collections": "Hobbies & Collections",
+  "humor-satire": "Humor & Satire",
+  "internet-technology": "Internet & Technology",
+  "love-confessions": "Love & Confessions",
+  "murders-assassinations": "[CENSORED]",
+  "personal-experiences": "Personal Experiences",
+  "pets-animals": "Pets & Animals",
+  "politics-elites": "Politics & [REDACTED]",
+  "sciences-nature": "Sciences & Nature",
+  "space-extraterrestrials": "Space & Extraterrestrials",
+  "supernatural-occultism": "Supernatural & Occultism",
+  "lost-media": "[CORRUPTED]"
+};
